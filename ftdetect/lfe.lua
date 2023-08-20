6172863a1f21e18ac2ca589f014e67c38c32c1f4
Jim Barnett <james.barnett.jr@gmail.com>
2023-08-20 01:12:29 -0500

    Adds group and autocmd to set commentstring for LFE

--- ftdetect/lfe.lua
+++ ftdetect/lfe.lua
@@ -1,6 +1,6 @@
 local api = vim.api
 
-local lfefileloaded = function(e)
+local lfefileloaded = function(ev)
   api.nvim_set_option_value("filetype", "lfe", {buf = 0})
 end
 
@@ -8,4 +8,15 @@ api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
   pattern = "*.lfe",
   callback = lfefileloaded,
 })
+
+local lfegroupid = api.nvim_create_augroup("LFE", {
+  clear = false
+})
+
+api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+  pattern = {"*.lfe"},
+  group = lfegroupid,
+  callback = function(ev)
+    vim.api.nvim_buf_set_option(0, "commentstring", "; %s")
+  end})
 
